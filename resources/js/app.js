d1962d61a4cc6af94b2e51ccd2f3368b0f887d54
Felipe <fdamasio17@gmail.com>
2022-12-08 20:05:21 -0300

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import './bootstrap';
+import $ from 'jquery';
+
+$('.user-link').click(function(e) {
+
+    var linkId = $(this).data('link-id');
+    var linkUrl = $(this).attr('href');
+
+    // store the visit asynchronously without interrupting the link opening
+    axios.post('/visit/' + linkId, {
+        link: linkUrl
+    })
+        .then(response => console.log('response: ', response))
+        .catch(error => console.error('error: ', error));
+
+});
